b8edd1342542e5fa6e8b90b744c1ffa4cc2133bc
filippaleuckfeld <filippaleuckfeld@gmail.com>
2023-12-15 15:25:08 +0100

    Fixed housing contract

--- contracts/HousingContract.sol
+++ contracts/HousingContract.sol
@@ -2,46 +2,33 @@
 pragma solidity ^0.8.9;
 
 contract HousingRentalContract {
-    // Contract owner (landlord)
     address public landlord;
-
-    // Renter of the property
     address public renter;
 
-    // Rental property details
-    string public propertyAddress;
-    //bool public isFurnished;
-    //bool public hasStorage;
-    
     // Rental terms
     uint public startTimestamp;
     uint public endTimestamp;
     uint public monthlyRent;
-    //enum Utilities { Electricity, Water, Internet, Heating }
-    //Utilities[] public includedUtilities;
+    uint public rentalObject;
+
+    // Add something to keep track on payments
+    uint public rentPaid;
+    uint public totalRent;
 
     // Constructor to set initial values
     constructor(
         address _renter,
-        string memory _propertyAddress,
-        //bool _isFurnished,
-        //bool _hasStorage,
+        uint _rentalObject,
         uint _startTimestamp,
         uint _endTimestamp,
         uint _monthlyRent
-        //Utilities[] memory _includedUtilities
     ) {
         landlord = msg.sender;
         renter = _renter;
-        propertyAddress = _propertyAddress;
-        //isFurnished = _isFurnished;
-        //hasStorage = _hasStorage;
+        rentalObject = _rentalObject;
         startTimestamp = _startTimestamp;
         endTimestamp = _endTimestamp;
         monthlyRent = _monthlyRent;
-        //for (uint i = 0; i < _includedUtilities.length; i++) {
-        //    includedUtilities.push(_includedUtilities[i]);
-        //}
     }
 
     // Function to terminate the contract
@@ -50,13 +37,15 @@ contract HousingRentalContract {
         endTimestamp = block.timestamp; // Update the end timestamp to current time
     }
 
-    // Function to pay rent (simplified version)
     function payRent() public payable {
         require(msg.sender == renter, "Only the renter can pay the rent");
         require(msg.value == monthlyRent, "Incorrect rent amount");
         payable(landlord).transfer(msg.value);
+        rentPaid += msg.value;
+    }
+
+    function contractFulfilled() public view returns (bool) {
+        return totalRent <= rentPaid;
     }
     
-    // Additional functions for contract management can be added here
-    // ...
 }
