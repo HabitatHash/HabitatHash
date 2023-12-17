cd87597ad7f668acc1cb4bc92e5c52e34d14824e
baggman <baggman@kth.se>
2023-12-17 18:43:10 +0100

    insurance contracts now go from usd to wei

--- contracts/HabitatHub.sol
+++ contracts/HabitatHub.sol
@@ -218,7 +218,7 @@ contract HabitatHub {
             msg.sender,
             rentalContractAddress,
             description,
-            value
+            getUsdToWei(value)
         );
 
         rentalContracts[rentalContractAddress].insuranceContract = address(
